7e21e3a20ef9cd86339f0121037e119561ab6d53
88ll88 <lumingxian666@gmail.com>
2026-02-16 11:05:09 +0800

    Update allTimes.js

--- allTimes.js
+++ allTimes.js
@@ -1,28 +1,31 @@
-// 計算函式 (結構完全統一)
 function times(group) {
-    var s = {};
-    for (var n = 1; n <= 39; n++) s[n.toString().padStart(2, '0')] = new Array(40).fill(0);
-    group.forEach(function(k) {
-        var d = window[k]; if (!d) return;
-        for (var i = 1; i < d.length; i++) {
-            d[i].forEach(function(h) {
-                var key = h.toString().padStart(2, '0');
-                d[i-1].forEach(function(t) { s[key][parseInt(t)]++; });
+    var 表 = {};
+    for (var n = 1; n <= 39; n++) 表[n.toString().padStart(2, '0')] = new Array(40).fill(0);
+
+    group.forEach(function(年) {
+        if (!年 || 年.length < 2) return;
+        for (var i = 1; i < 年.length; i++) {
+            var 舊 = 年[i].de || 年[i];
+            var 新 = 年[i-1].de || 年[i-1];
+            舊.forEach(function(h) {
+                var 號 = h.toString().padStart(2, '0');
+                新.forEach(function(t) { 表[號][parseInt(t)]++; });
             });
         }
     });
-    var res = {};
-    for (var k in s) res[k] = s[k].slice(1);
-    return JSON.stringify(res);
+
+    var 結果 = {};
+    for (var k in 表) 結果[k] = 表[k].slice(1);
+    return JSON.stringify(結果);
 }
 
-// --- 執行以下指令，分別複製結果去存檔 ---
+// --- 休息好了再跑這三行，複製結果去存檔 ---
 
-// 1. 存成 2007-2025.js
-console.log(times("TSRQPONMLKJIHGFEDCB".split("")));
+// 1. 歷史數據 (T, S, R... 到 B)
+console.log("2007-2025：", times([T, S, R, Q, P, O, N, M, L, K, J, I, H, G, F, E, D, C, B]));
 
-// 2. 存成 2024-今.js (包含 2026/A)
-console.log(times(["C", "B", "A"]));
+// 2. 2024-2026 (C, B, A)
+console.log("2024-今：", times([C, B, A]));
 
-// 3. 存成 2025-今.js (包含 2026/A)
-console.log(times(["B", "A"]));
+// 3. 2025-2026 (B, A)
+console.log("2025-今：", times([B, A]));
