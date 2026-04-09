91364ab874e2d5eb774958207727b843768c41a1
Lu <lumingxian888@gmail.com>
2026-04-09 10:21:51 +0800

    Add files via upload

---
+++ 6/xyz.js
@@ -0,0 +1,8 @@
+var navBar='<div class="nav-bar">';
+       navBar+='<a href="分佈圖.html" class="nav-tab">分佈圖</a>';
+       navBar+='<a href="柱狀圖.html" class="nav-tab">柱狀圖</a>';
+       navBar+='<a href="三分區.html" class="nav-tab">三分區</a>';
+       navBar+='<a href="拖牌.html" class="nav-tab">拖牌</a>';
+       navBar+='<a href="遺漏.html" class="nav-tab active">遺漏</a>';
+       navBar+='<a href="統計表.html" class="nav-tab">統計表</a>';
+       navBar+='</div>';
